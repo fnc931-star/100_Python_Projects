9383e683b70f8ac220a67438efb402dbef791113
fnc931-star <fnc931@gmail.com>
2026-04-01 12:11:22 +0500

    Update the file name and also add some code lines into GUI_app file

--- 001_Band_Name_Generator/gui_app.py
+++ 001_Band_Name_Generator/gui_app.py
@@ -6,5 +6,18 @@ root = tk.Tk()
 # It represent the Window screen heading
 root.title("Band Name Generator")
 
+<<<<<<< HEAD
 
 root.mainloop()
+=======
+root.geometry("700x700")
+
+btn = tk.Button(root, text="Run",command=root.destroy ,disabledforeground='grey',anchor="center",font=("Arial",12),justify='left',overrelief="flat",compound='left',border=0,padx=5,pady=5,width=10)
+btn.pack(side='left')
+
+
+root.mainloop()
+root, 
+      
+    
+>>>>>>> b5d34a6 (Update the file name and also add some code lines into GUI_app file)
