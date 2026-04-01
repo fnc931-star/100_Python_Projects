b6285ac3363279544589a0ab21ec8d3c6c9fd8e3
Coder <fnc931@gmail.com>
2026-04-01 12:29:04 +0500

    Update gui_app.py
    few of the lines were wrong so i remove those lines and update the file

--- 001_Band_Name_Generator/gui_app.py
+++ 001_Band_Name_Generator/gui_app.py
@@ -4,12 +4,8 @@ import tkinter as tk
 # Create a main application window screen
 root = tk.Tk()
 # It represent the Window screen heading
-root.title("Band Name Generator")
-
-<<<<<<< HEAD
-
-root.mainloop()
-=======
+root.title("Band Name Generator
+           
 root.geometry("700x700")
 
 btn = tk.Button(root, text="Run",command=root.destroy ,disabledforeground='grey',anchor="center",font=("Arial",12),justify='left',overrelief="flat",compound='left',border=0,padx=5,pady=5,width=10)
@@ -17,7 +13,3 @@ btn.pack(side='left')
 
 
 root.mainloop()
-root, 
-      
-    
->>>>>>> b5d34a6 (Update the file name and also add some code lines into GUI_app file)
