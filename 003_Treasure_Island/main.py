cf18af4bf0b3b9ac514650c7b315ba0e34b96779
fnc931-star <fnc931@gmail.com>
2026-04-08 02:14:12 +0500

    Started my new project 003 'Tressure Island" its kind of a game without visual arts but i add some ascii visual arts to look nice. In this using some basic conditional rules of python.

---
+++ 003_Treasure_Island/main.py
@@ -0,0 +1,23 @@
+import os
+from time import sleep
+import visual_art
+
+
+if __name__ == '__main__':
+    heading, treasure_box = visual_art.ascii_art() 
+    print(heading+"\n")
+    print(treasure_box+"\n")
+
+    print("\nWELCOME TO TREASURE ISLAND")
+    print("Your MISSION is to find the Treasure")
+
+
+    print("Thank You for visiting My Island")
+
+    sleep(5)
+
+    os.system('clear')
+
+
+
+
