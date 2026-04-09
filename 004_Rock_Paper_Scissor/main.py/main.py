63da907ee61fcf44c69ee0edd11381e453646622
fnc931-star <fnc931@gmail.com>
2026-04-09 21:39:57 +0500

    Complete the Project, This is now fully functional Rocking Game

--- 004_Rock_Paper_Scissor/main.py/main.py
+++ 004_Rock_Paper_Scissor/main.py/main.py
@@ -1,7 +1,26 @@
 # main file 
 import os
 from time import sleep
+import random
 import visual_arts
 import rocking_game
+
+
+if __name__ == "__main__":
+    print(f"\t\t {visual_arts.heading}")
+
+    user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor.\n"))
+    comp = random.randint(0,2)
+
+    print(rocking_game.art[user])
+    print("\nComputer Chose:\n",rocking_game.art[comp])
+
+    result = rocking_game.score(user,comp)
+
+    print(f"\n\t\t\t\t{result}\n")
+    print("\t\tThankYou For Playing My Rocking Game!😎🥰🤗")
+
+    sleep(5)
+    os.system('clear')
 
 
