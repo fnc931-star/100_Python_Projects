a72f2cd77fc6981c94e3beaf042b9b9f5f5ae77d
fnc931-star <fnc931@gmail.com>
2026-04-10 22:21:06 +0500

    Here i just add few lines of code so that i must check that project is working perfectly or not

---
+++ 005_PyPassword_Generator/main.py
@@ -0,0 +1,11 @@
+import os 
+from time import sleep
+import visual_art
+import pass_generator
+
+
+if __name__ == "__main__":
+    print(visual_art.heading)
+    print("\nWELCOME TO THE PYPASSWORD GENERATOR!\n")
+
+
