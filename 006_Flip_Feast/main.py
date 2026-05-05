df03247926dfbfbfec1934e6c5659102ac4b159b
fnc931-star <fnc931@gmail.com>
2026-05-05 23:34:51 +0500

    Logical Code Snippet add krdia hae ab Try kro or order Decision automate kro😆😉

---
+++ 006_Flip_Feast/main.py
@@ -0,0 +1,9 @@
+import random
+
+decision_maker = [random.randint(0,1) for i in range(3)]
+
+if decision_maker.count(0) > 1:
+    print("Sorry! Aj paisy bachao or Ghar ka Khana khao😂")
+
+else:
+    print("Wah Wah! Jan chuti 😅 Nikalo Pais 🤑 or Urao Fast Food pr,'\n' Mazy kro😋")
