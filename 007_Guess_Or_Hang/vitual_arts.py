50b1b4c37068b920c470b0e46a1dae5a031d3a1a
fnc931-star <fnc931@gmail.com>
2026-06-16 10:37:56 +0500

    modified few things

---
+++ 007_Guess_Or_Hang/vitual_arts.py
@@ -0,0 +1,66 @@
+# here I add the all ascii vitual arts for the Hangman project
+
+# This heading is taken from there by using standard fonts - https://patorjk.com/software/taag/#p=display&f=Standard&t=OR&x=none&v=4&h=4&w=80&we=false
+
+headings = """
+   ____ _   _ _____ ____ ____        _____ ____         _   _    _    _   _  ____ 
+  / ___| | | | ____/ ___/ ___|      /  _  \|  _ \      | | | |  / \  | \ | |/ ___|
+ | |  _| | | |  _| \___ \___ \     |  | |  | |_) |     | |_| | / _ \ |  \| | |  _ 
+ | |_| | |_| | |___ ___) |__) |    |  |_|  |  _ <      |  _  |/ ___ \| |\  | |_| |
+  \____|\___/|_____|____/____/      \_____/|_| \_\     |_| |_/_/   \_\_| \_|\____|              
+                               
+                   """
+
+
+# These are the hang man pics which are taken from there : https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
