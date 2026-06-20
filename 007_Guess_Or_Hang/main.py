282f457441eb8d40f3e1b38f4c5b8498682e3449
fnc931-star <fnc931@gmail.com>
2026-06-20 20:45:48 +0500

    Let's play the game🥳.

--- 007_Guess_Or_Hang/main.py
+++ 007_Guess_Or_Hang/main.py
@@ -1,7 +1,18 @@
 import os
 import vitual_arts
-
+import time 
+import hangman
 
 if __name__ == "__main__":
+
     heading = vitual_arts.headings 
     print(heading)
+
+    word = hangman.word  
+    hangman.guess_or_hang(word)
+
+
+    print(f"ThankYou for Using my '{'guess or hang Game'.upper()}")
+    time.sleep(7)
+    os.system('clear')
+    
